add6b31333102aaad21a64f8909bc3aa6eea38a8
Emmanuels-code <emmaokogho@yahoo.com>
2024-10-01 16:26:52 +0100

    design changes

--- src/App.js
+++ src/App.js
@@ -13,17 +13,21 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <h1>Book-For-Me: Book Recommender</h1>
-        </header>
-        <main>
-          <Routes>
-            {/* Use element prop and wrap components in JSX */}
-            <Route path="/" element={<CategorySelector setCategory={setCategory} />} />
-            <Route path="/page-count" element={<PageCountSelector setPageCount={setPageCount} />} />
-            <Route path="/suggestions" element={<BookList category={category} pageCount={pageCount} />} />
-          </Routes>
-        </main>
+        {/* Background Image and Overlay */}
+        <div className="background-container">
+          <div className="background-overlay"></div>
+          <header className="App-header">
+            <h1>Book For Me</h1>
+            <h2> Book For you!</h2>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<CategorySelector setCategory={setCategory} />} />
+              <Route path="/page-count" element={<PageCountSelector setPageCount={setPageCount} />} />
+              <Route path="/suggestions" element={<BookList category={category} pageCount={pageCount} />} />
+            </Routes>
+          </main>
+        </div>
       </div>
     </Router>
   );
